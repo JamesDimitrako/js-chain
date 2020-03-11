e5a4d36afb4c1faa3cec74d53bdf3706bb7f9e54
JamesDimitrako <15272980+JamesDimitrako@users.noreply.github.com>
2020-03-11 23:33:03 +0200

    Mine Blocks

--- dev-test.js
+++ dev-test.js
@@ -1,10 +1,15 @@
 const Block = require('./block');
 
-const block = new Block('foo', 'bar', 'zoo', 'baz');
+// previous tests
+// const block = new Block('foo', 'bar', 'zoo', 'baz');
 
-console.log(block.toString());
+// console.log(block.toString());
 
-console.log(Block.genesis().toString());
+// console.log(Block.genesis().toString());
 
 
-console.log("----------------------");
+// console.log("----------------------");
+
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+
+console.log(fooBlock.toString());
